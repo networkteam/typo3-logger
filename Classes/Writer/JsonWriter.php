bc02ffbea7efa9f865e299f96982ea6692847991
Christoph Lehmann <christoph.lehmann@networkteam.com>
2024-02-09 22:01:17 +0100

    Initial commit

---
+++ Classes/Writer/JsonWriter.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Networkteam\Logger\Writer;
+
+use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Log\LogRecord;
+use TYPO3\CMS\Core\Log\Writer\AbstractWriter;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Log writer that writes the log records to stderr
+ */
+class JsonWriter extends AbstractWriter
+{
+    protected array $options;
+
+    public function __construct(array $options = [])
+    {
+        $this->options = $options;
+    }
+
+    public function writeLog(LogRecord $record)
+    {
+        $context = $record->getData();
+        $message = $this->interpolate($record->getMessage(), $context);
+        $throwable = $context['exception'] ?? null;
+
+        if ($throwable instanceof \Throwable) {
+            $message = sprintf(
+                '%s: %s',
+                $context['exception_class'] ?? $this->getExceptionClass($throwable),
+                $throwable->getMessage()
+            );
+            $context['file'] = str_replace(Environment::getProjectPath() . '/', '', $throwable->getFile());
+            unset(
+                $context['exception'],
+                $context['exception_class'],
+                $context['message'],
+                $context['request_url']
+            );
+        }
+
+        $data = [
+            'time' => date('r', (int)$record->getCreated()),
+            'severity' => $record->getLevel(),
+            'message' => $message,
+            'component' => $record->getComponent(),
+            'source' => 'typo3',
+            'typo3_request_id' => $record->getRequestId(),
+            'context' => $context
+        ];
+
+        $method = $_SERVER['REQUEST_METHOD'] ?? null;
+        if ($method) {
+            $data['url'] = $this->anonymizeToken(GeneralUtility::getIndpEnv('TYPO3_REQUEST_URL'));
+            $data['method'] = $method;
+            $requestId = $_SERVER['X-REQUEST-ID'] ?? $_SERVER['HTTP_X_REQUEST_ID'] ?? null;
+            if ($requestId) {
+                $data['request_id'] = $requestId;
+            }
+        }
+
+        if ($this->options !== []) {
+            $data['logger_context'] = $this->options;
+        }
+
+        $stderr = fopen('php://stderr', 'a');
+        fwrite($stderr, json_encode($data) . PHP_EOL);
+        fclose($stderr);
+
+        return $this;
+    }
+
+    protected function getExceptionClass(\Throwable $throwable): string
+    {
+        $classname = get_class($throwable);
+        if ($pos = strrpos($classname, '\\')) {
+            $classname = substr($classname, $pos + 1);
+        }
+        return $classname;
+    }
+
+    /**
+     * Replaces the generated token with a generic equivalent
+     *
+     * @param string $requestedUrl
+     * @return string
+     */
+    protected function anonymizeToken(string $requestedUrl): string
+    {
+        $pattern = '/(?:(?<=[tT]oken=)|(?<=[tT]oken%3D))[0-9a-fA-F]{40}/';
+        return preg_replace($pattern, '--AnonymizedToken--', $requestedUrl);
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,71 @@
+# TYPO3 Logger
+
+This extension provides a LogWriter to log in JSON format to stderr.
+
+## Installation
+
+Run `composer req networkteam/typo3-logger`
+
+## Configuration
+
+Basic configuration
+
+```yaml
+LOG:
+  writerConfiguration:
+    warning:
+      Networkteam\Logger\Writer\JsonWriter:
+        foo: 'bar'
+```
+
+In container environments it might be handy to register the JsonWriter as XClass for the FileWriter. This way no log
+files are written to `typo3temp/var/log/`.
+
+```yaml
+SYS:
+  Objects:
+    TYPO3\CMS\Core\Log\Writer\FileWriter:
+      className: 'Networkteam\Logger\Writer\JsonWriter'
+```
+
+## Example output
+
+```json
+{
+  "time": "Fri, 09 Feb 2024 21:21:49 +0100",
+  "severity": "critical",
+  "message": "Exception: test",
+  "component": "TYPO3.CMS.Core.Error.DebugExceptionHandler",
+  "source": "typo3",
+  "typo3_request_id": "6be0a6b53348f",
+  "context": {
+    "mode": "WEB",
+    "application_mode": "BE",
+    "exception_code": 0,
+    "file": "typo3/typo3/sysext/backend/Classes/Middleware/BackendUserAuthenticator.php",
+    "line": 95
+  },
+  "url": "http://localhost:8080/typo3/module/system/config",
+  "method": "GET",
+  "logger_context": {
+    "foo": "bar"
+  }
+}
+```
+```json
+{
+  "time": "Fri, 09 Feb 2024 21:23:07 +0100",
+  "severity": "warning",
+  "message": "Illegal filepath \"EXT:calendarize/Configuration/TypoScript/setup.typoscript\".",
+  "component": "TYPO3.CMS.Core.TypoScript.Parser.TypoScriptParser",
+  "source": "typo3",
+  "typo3_request_id": "c429ef02dcf9d",
+  "external_request_id": "96a101dd-c49a-4fea-aee2-a76510f32190",
+  "context": [],
+  "url": "http://localhost:8080/typo3/module/system/config?token=--AnonymizedToken--",
+  "method": "GET",
+  "logger_context": {
+    "foo": "bar"
+  }
+}
+```
